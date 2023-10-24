f246a525e27575ce2b525b5668282e53e5443512
Nicolas Perrault <nipe1783@colorado.edu>
2023-10-23 20:45:29 -0600

    working

--- OpenCVDemo/main.cpp
+++ OpenCVDemo/main.cpp
@@ -2,15 +2,17 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <torch/torch.h>
 
 // tutorial files
 #include "videoDemo/VideoDemo.cpp"
 #include "imageDemo/ImageDemo.cpp"
 #include "webCamDemo/WebCamDemo.cpp"
+#include "torchDemo/TorchDemo.cpp"
 
 using namespace cv;
 int main()
 {
-    showWebCam();
+    test();
     return 0;
 }
---
+++ OpenCVDemo/torchDemo/TorchDemo.cpp
@@ -0,0 +1,13 @@
+#include <torch/torch.h>
+#include <iostream>
+#include "../include/network.h"
+
+
+using namespace torch;
+void test(){
+    Net network(50,50);
+    std::cout << network << std::endl;
+    Tensor x, output;
+    x = torch::randn({2,50});
+    output = network->forward(x);
+}
