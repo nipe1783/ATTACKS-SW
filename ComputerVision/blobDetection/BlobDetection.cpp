af60a51378942ec3965a955c77fbcf831410d31c
Nicolas Perrault <nipe1783@colorado.edu>
2023-10-30 21:45:42 -0600

    basic filtering

--- ComputerVision/blobDetection/BlobDetection.cpp
+++ ComputerVision/blobDetection/BlobDetection.cpp
@@ -9,31 +9,53 @@ void BlobDetection::calibrate(std::string imagePath)
 {
     std::cout << "Calibrating..." << std::endl;
 
-    Mat image = imread(imagePath, IMREAD_COLOR);
-    Mat filteredImage;
+    Mat frame = imread(imagePath, IMREAD_COLOR);
+    Mat frameHSV;
+    Mat filteredFrame;
 
     namedWindow("Original Frame", WINDOW_AUTOSIZE);
     namedWindow("Filtered Frame", WINDOW_AUTOSIZE);
     
-    imshow("Original Frame", image);
+    imshow("Original Frame", frame);
     
-    cvtColor(image, filteredImage, COLOR_BGR2HSV);
+    cvtColor(frame, frameHSV, COLOR_BGR2HSV);
     createTrackbar("Low H", "Filtered Frame", &hLow, maxValueH, on_low_H_thresh_trackbar, this);
     createTrackbar("High H", "Filtered Frame", &hHigh, maxValueH, on_high_H_thresh_trackbar, this);
+    createTrackbar("Low S", "Filtered Frame", &sLow, maxValueH, on_low_S_thresh_trackbar, this);
+    createTrackbar("High S", "Filtered Frame", &sHigh, maxValueH, on_high_S_thresh_trackbar, this);
+    createTrackbar("Low V", "Filtered Frame", &vLow, maxValueH, on_low_V_thresh_trackbar, this);
+    createTrackbar("High V", "Filtered Frame", &vHigh, maxValueH, on_high_V_thresh_trackbar, this);
+    
+    imshow("HSV Frame", frameHSV);
 
-    imshow("Filtered Frame", filteredImage);
-    
-    waitKey(0);
-    
+    while(true){
+        // Detect the object based on HSV Range Values
+        inRange(frameHSV, Scalar(hLow, sLow, vLow), Scalar(hHigh, sHigh, vHigh), filteredFrame);
+        
+        // Show the filtered frame
+        imshow("Filtered Frame", filteredFrame);
+
+        char key = (char) waitKey(30);
+        if (key == 'q' || key == 27) // 'q' or 'ESC' key
+        {
+            break;
+        }
+    }
+    std::cout << "Low H: " << hLow << std::endl;
+    std::cout << "High H: " << hHigh << std::endl;
+    std::cout << "Low S: " << sLow << std::endl;
+    std::cout << "High S: " << sHigh << std::endl;
+    std::cout << "Low V: " << vLow << std::endl;
+    std::cout << "High V: " << vHigh << std::endl;
     std::cout << "Finished Calibrating." << std::endl;
 }
+
 
 void BlobDetection::detect(std::string imagePath)
 {
     std::cout << "Detecting..." << std::endl;
 }
 
-// Static callback methods
 void BlobDetection::on_low_H_thresh_trackbar(int pos, void* userdata)
 {
     BlobDetection* instance = (BlobDetection*)userdata;
@@ -46,4 +68,32 @@ void BlobDetection::on_high_H_thresh_trackbar(int pos, void* userdata)
     BlobDetection* instance = (BlobDetection*)userdata;
     instance->hHigh = max(instance->hHigh, instance->hLow+1);
     setTrackbarPos("High H", "Filtered Frame", instance->hHigh);
+}
+
+void BlobDetection::on_low_S_thresh_trackbar(int pos, void* userdata)
+{
+    BlobDetection* instance = (BlobDetection*)userdata;
+    instance->sLow = min(instance->sHigh-1, instance->sLow);
+    setTrackbarPos("Low S", "Filtered Frame", instance->sLow);
+}
+
+void BlobDetection::on_high_S_thresh_trackbar(int pos, void* userdata)
+{
+    BlobDetection* instance = (BlobDetection*)userdata;
+    instance->sHigh = max(instance->sHigh, instance->sLow+1);
+    setTrackbarPos("High S", "Filtered Frame", instance->sHigh);
+}
+
+void BlobDetection::on_low_V_thresh_trackbar(int pos, void* userdata)
+{
+    BlobDetection* instance = (BlobDetection*)userdata;
+    instance->vLow = min(instance->vHigh-1, instance->vLow);
+    setTrackbarPos("Low V", "Filtered Frame", instance->vLow);
+}
+
+void BlobDetection::on_high_V_thresh_trackbar(int pos, void* userdata)
+{
+    BlobDetection* instance = (BlobDetection*)userdata;
+    instance->vHigh = max(instance->vHigh, instance->vLow+1);
+    setTrackbarPos("High V", "Filtered Frame", instance->vHigh);
 }
--- ComputerVision/blobDetection/BlobDetection.h
+++ ComputerVision/blobDetection/BlobDetection.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 class BlobDetection
 {
@@ -8,6 +13,10 @@ private:
     // Static callback methods for trackbars:
     static void on_low_H_thresh_trackbar(int, void*);
     static void on_high_H_thresh_trackbar(int, void*);
+    static void on_low_S_thresh_trackbar(int, void*);
+    static void on_high_S_thresh_trackbar(int, void*);
+    static void on_low_V_thresh_trackbar(int, void*);
+    static void on_high_V_thresh_trackbar(int, void*);
 
 public:
     // Constructor and destructor:
@@ -17,6 +26,7 @@ public:
     // Public methods:
     void calibrate(std::string imagePath);
     void detect(std::string imagePath);
+    void updateFilteredImage(const Mat& src, Mat& dest);
     
     // Member variables:
     int hLow = 0;
