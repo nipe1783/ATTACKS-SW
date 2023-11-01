cdd190e23259cc37b905b9a939a1ed3e6fd52680
Nicolas Perrault <nipe1783@colorado.edu>
2023-11-01 14:05:04 -0600

    adding datset

--- ComputerVision/main.cpp
+++ ComputerVision/main.cpp
@@ -14,35 +14,11 @@ using namespace cv;
 int main()
 {
     BlobDetection blobDetector;
-    std::string videoPath = "../videos/test.mp4";
-    VideoCapture cap(videoPath);
-    Mat frame;
+    std::string imagePath = "../images/EORSSD/test-images/0004.jpg"; 
+    Mat frame = imread(imagePath);
     Mat filteredFrame;
-    int counter = 0;
-    while(true){
-        cap >> frame;
-        if(frame.empty()){
-            std::cout << "End of video." << std::endl;
-            break;
-        }
-        if(counter == 0){
-            blobDetector.calibrate(frame);
-            counter++;
-        }
-        cvtColor(frame, filteredFrame, COLOR_BGR2HSV);
-        GaussianBlur(filteredFrame, filteredFrame, Size(blobDetector.blurSize, blobDetector.blurSize), 0);
-        inRange(filteredFrame, Scalar(blobDetector.hLow, blobDetector.sLow, blobDetector.vLow), Scalar(blobDetector.hHigh, blobDetector.sHigh, blobDetector.vHigh), filteredFrame);
-        std::shared_ptr<Blob> blob = blobDetector.detect(frame);
-        if(blob != nullptr){
-            rectangle(frame, Point(blob->x, blob->y), Point(blob->x + blob->width, blob->y + blob->height), Scalar(0, 255, 0), 2);
-        }
-        Visualizer::twoFrame(frame, filteredFrame);
-        char key = (char) waitKey(30);
-        if (key == 'q' || key == 27) // 'q' or 'ESC' key
-        {
-            break;
-        }
-    }
 
+    imshow("Original", frame);
+    waitKey(0);
     return 0;
 }
