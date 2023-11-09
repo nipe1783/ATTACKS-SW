bffdcde1f10aca15d4ecd3e7df7cc7ac968ea7c1
Alex Lam <alla1028@colorado.edu>
2023-11-08 19:03:50 -0700

    Adding binaryImage.py script

--- ComputerVision/main.cpp
+++ ComputerVision/main.cpp
@@ -13,6 +13,11 @@
 
 
 using namespace cv;
+
+struct data {
+    float x1,y1,x2,y2,x3,y3,x4,y4;
+};
+
 int main()
 {
     Scripts script;
@@ -22,6 +27,24 @@ int main()
     
     Scripts::videoRunner("DroneTestFootage.mp4", basicBlobDetector);
     // Scripts::cameraRunner(0, basicBlobDetector);
+
+    // Saving every 50 frames as an image
+    // std::string videoPath = "/home/alex/Documents/ATTACKS-SW/ComputerVision/videos/DroneTestFootage.mp4";
+    // VideoCapture cap(videoPath);
+    // Mat frame;
+    // std::string name;
+    // int counter = 0;
+    // while(true){
+    //     cap >> frame;
+    //     if(counter%50 == 0){
+    //         name = "frame_" + std::to_string(counter) + ".png";
+    //         imwrite("/home/alex/Documents/ATTACKS-SW/ComputerVision/images/" + name, frame);
+    //     }
+    //     counter++;
+    //     if(frame.empty()){
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
---
+++ ComputerVision/scripts/binaryImage.py
@@ -0,0 +1,35 @@
+import json
+import cv2 as cv
+import numpy as np
+
+# Load in json file
+f = open('/home/alex/Downloads/rgv.json')
+data = json.load(f)
+
+xLocation = []
+yLocation = []
+
+# Gather all x and y locations
+for filename in data:
+    for x in data[filename]['regions'][0]['shape_attributes']['all_points_x']:
+        xLocation.insert(0,x)
+    for y in data[filename]['regions'][0]['shape_attributes']['all_points_y']:
+        yLocation.insert(0,y)
+
+# Close json file
+f.close()
+
+# Match x and y together
+pairs = np.array(list(zip(xLocation,yLocation)))
+# Create a subarray for every four pairs
+groupedPairs = np.split(pairs,41)
+
+img = cv.imread("../images/frame_0.png")
+frames = range(0,2050,50)
+for i in range(0,len(frames)):
+    # Create black image
+    filled = np.zeros_like(img)
+    # Draw white polygon with specified coordinates
+    filled = cv.fillPoly(filled, pts = [groupedPairs[i]], color = (255,255,255))
+    # Save image
+    cv.imwrite("../images/binaryFrame_%s.png" % frames[i], filled)
