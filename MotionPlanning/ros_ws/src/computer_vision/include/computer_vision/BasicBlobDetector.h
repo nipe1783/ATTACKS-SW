79f7dc2c84a660696fa33a6d5decd736694e8cfd
Nic Perrault <nipe1783@colorado.edu>
2024-01-10 19:42:03 -0500

    moving computer vision code to ros library

---
+++ MotionPlanning/ros_ws/src/computer_vision/include/computer_vision/BasicBlobDetector.h
@@ -0,0 +1,16 @@
+# pragma once
+#include "computer_vision/BlobDetector.h"
+#include "computer_vision/Blob.h"
+#include <vector>
+
+using namespace cv;
+
+class BasicBlobDetector : public BlobDetector
+{       
+    public:
+        // methods:
+        BasicBlobDetector() = default;
+        ~BasicBlobDetector() = default;
+        std::vector<Blob> detect(Mat& frame, Mat& dst) override;
+        void calibrate(Mat& frame) override;
+};
