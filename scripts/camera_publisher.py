263205d440552dad23d57dd4f351d2b3ee352575
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-11 16:25:18 -0600

    test

--- scripts/camera_publisher.py
+++ scripts/camera_publisher.py
@@ -10,7 +10,7 @@ from cv_bridge import CvBridge
 class CameraPublisher(Node):
     def __init__(self):
         super().__init__('camera_publisher')
-        self.publisher_ = self.create_publisher(Image, '/camera/image_raw', 10)
+        self.publisher_ = self.create_publisher(Image, '/camera/image_raw', 5)
         self.timer_period = 1/30 
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.bridge = CvBridge()
