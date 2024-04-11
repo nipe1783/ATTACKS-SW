3d402d768016faa25b69895833cab538db419dcb
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-11 16:02:00 -0600

    Test

--- scripts/camera_publisher.py
+++ scripts/camera_publisher.py
@@ -3,8 +3,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
+from cv_bridge import CvBridge, CvBridgeError
 
 class CameraPublisher(Node):
     def __init__(self):
