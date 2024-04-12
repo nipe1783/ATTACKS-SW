3473088cd444ca84d68e77719be7f7170e0f6cf6
Nicolas Perrault <nipe1783@colorado.edu>
2024-04-11 21:05:24 -0600

    test

--- scripts/usb_camera_show.py
+++ scripts/usb_camera_show.py
@@ -1,16 +1,14 @@
+
 import cv2
 import datetime
 import os
 
-def gstreamer_pipeline(capture_width=1920, capture_height=1080, display_width=1920, display_height=1080, framerate=30, flip_method=0):
+def gstreamer_pipeline(capture_width=1280, capture_height=720, display_width=1280, display_height=720, framerate=30, flip_method=0):
     return (
-        f"v4l2src device=/dev/video0 ! "
-        f"video/x-raw, width=(int){capture_width}, height=(int){capture_height}, format=(string)YUY2, framerate=(fraction){framerate}/1 ! "
-        f"videoconvert ! "
-        f"video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGR ! "
-        f"videoflip method={flip_method} ! "
-        "appsink drop=true sync=false"
-    )
+        "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int){}, height=(int){}, format=(string)NV12, framerate=(fraction){}/1 ! "
+        "nvvidconv flip-method={} ! video/x-raw, width=(int){}, height=(int){}, format=(string)BGRx ! "
+        "videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=true sync=false"
+    ).format(capture_width, capture_height, framerate, flip_method, display_width, display_height)
 
 def make_video_writer(width, height, fps=30, recordings_dir="Recordings"):
     recordings_path = os.path.join(os.getcwd(), recordings_dir)
@@ -19,19 +17,17 @@ def make_video_writer(width, height, fps=30, recordings_dir="Recordings"):
 
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     filename = os.path.join(recordings_path, f"Recording_{current_time}.mp4")
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
     out = cv2.VideoWriter(filename, fourcc, fps, (width, height))
     return out
-
-cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=1), cv2.CAP_GSTREAMER)
 if not cap.isOpened():
     print("Failed to open camera.")
     exit()
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
-video_writer = make_video_writer(width, height, fps)
+video_writer = make_video_writer(width, height)
 
 try:
     while True:
@@ -39,9 +35,10 @@ try:
         if not ret:
             print("Failed to capture frame.")
             break
-
+        
         video_writer.write(frame)
-        cv2.imshow("USB Camera", frame)
+        
+        cv2.imshow("CSI Camera", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
